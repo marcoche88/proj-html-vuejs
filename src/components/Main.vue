d40a892d88dc6f0dfa65bfaef794e943a241f8ef
marcoche88 <marco.che@hotmail.it>
2021-09-16 11:13:21 +0200

    added section coding lang and box component

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,70 +1,80 @@
 <template>
   <main>
     <!-- jumbotron -->
-    <section id="main-hero" class="py-5 position-relative">
-      <div class="my-container container h-100 position-relative">
-        <div class="row h-100">
-          <div class="col-6">
-            <div class="text-hero w-50 mt-5">
-              <h2>Complete Your Dreams in JavaS Programming</h2>
-              <p class="my-4">
-                Learn any coding program in distance and for a reasonable fee.
-                You don't have to struggle alone, you've got our assistance and
-                help.
-              </p>
-              <button type="button" class="btn px-4 py-2">
-                <i class="fas fa-download"></i> Download free E-book
-              </button>
-            </div>
-          </div>
-          <div class="col-6 h-100 text-end">
-            <img
-              class="h-100"
-              src="../assets/images/home-5-hero-image.png"
-              alt="main-hero"
-            />
+    <MainHero />
+
+    <!-- section coding language -->
+    <section id="coding-languages" class="my-5">
+      <p class="text-center text-uppercase">Start learning coding languages</p>
+      <h2 class="text-center">
+        Build Your Dream <span class="text-uppercase fw-light">today</span>
+      </h2>
+      <div class="my-container container mt-5">
+        <div class="row">
+          <div class="col-3" v-for="box in boxes" :key="box.img">
+            <Box :box="box" />
           </div>
         </div>
       </div>
-      <div class="wave position-absolute"></div>
     </section>
   </main>
 </template>
 
 <script>
+import MainHero from "./main/MainHero.vue";
+import Box from "./main/Box.vue";
 export default {
   name: "Main",
+  components: {
+    MainHero,
+    Box,
+  },
+  data() {
+    return {
+      boxes: [
+        {
+          img: "01",
+          title: "Idea Discussion",
+          text: "Get teamed up with the specialists who work and teach coding for years at famous universities.",
+          link: "#",
+        },
+        {
+          img: "02",
+          title: "Web Development",
+          text: "Learn to start building a webpage from scratch. You decide your own pace, course and speed.",
+          link: "#",
+        },
+        {
+          img: "03",
+          title: "System Administration",
+          text: "Learners are encouraged to study the mechanism and structure of system administration.",
+          link: "#",
+        },
+        {
+          img: "04",
+          title: "Graphic Design",
+          text: "Have a passion for graphics and arts? Show your talents with confidence and self-assertiveness.",
+          link: "#",
+        },
+      ],
+    };
+  },
 };
 </script>
 
 <style scoped lang="scss">
 @import "../scss/_vars.scss";
 
-#main-hero {
-  background-color: $bg-primary-color;
-  height: 600px;
-  color: $text-tertiary-color;
-  .my-container {
-    z-index: 1;
-  }
+#coding-languages {
   p {
     font-size: 11px;
-    line-height: 20px;
+    color: $text-secondary-color;
   }
-  button {
-    font-size: 11px;
-    font-weight: bold;
-    color: white;
-    background-color: $text-primary-color;
-  }
-
-  .wave {
-    height: 400px;
-    background-image: url("../assets/images/jumbo-overlay.svg");
-    transform: rotate(180deg) scaleX(-1);
-    bottom: 0;
-    right: 0;
-    left: 0;
+  h2 {
+    color: $text-tertiary-color;
+    span {
+      color: $text-primary-color;
+    }
   }
 }
 </style>
---
+++ src/components/main/Box.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="card border-0">
+    <img :src="ImgUrl" class="img-fluid px-5" :alt="box.title" />
+    <div class="card-body text-center">
+      <h6 class="card-title mb-3">{{ box.title }}</h6>
+      <p class="card-text">{{ box.text }}</p>
+      <a :href="box.link">Start now <i class="fas fa-arrow-right"></i></a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Box",
+  props: {
+    box: Object,
+  },
+  computed: {
+    ImgUrl() {
+      return require(`../../assets/images/home-5-image-box-${this.box.img}.png`);
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import "../../scss/_vars.scss";
+
+img {
+  max-height: 130px;
+}
+
+p {
+  font-size: 11px;
+  color: $text-secondary-color;
+}
+
+a {
+  display: block;
+  text-decoration: none;
+  font-size: 10px;
+  font-weight: bold;
+  color: $text-tertiary-color;
+}
+</style>
